b44dc33909a1f6b66117296de27bcd9d2f21e2cc
Prince1130-sam <princekuldeep2022@gmail.com>
2026-05-10 12:08:29 +0530

    Question 3 Print odd no

--- Phase_1_WhileLoop/Q.3.js
+++ Phase_1_WhileLoop/Q.3.js
@@ -1,10 +1,23 @@
 // Q.3 Print all even numbers between 1 and 100.
 
-let num = 1 ;
+// let num = 1 ;
 
-while(num <= 100){
-    if(num % 2 === 0){
-        console.log(num)
+// while(num <= 100){
+//     if(num % 2 === 0){
+//         console.log(num)
+//     }
+//     num++
+// }
+
+
+// Q.4 Print all odd numbers between 1 and 100.
+
+let odd = 1;
+
+while(odd <= 100){
+    if(odd % 2 !== 0){
+        console.log(odd)
+        odd++
     }
-    num++
 }
+
