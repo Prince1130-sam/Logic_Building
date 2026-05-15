2871f983ff983fff58a97435e92b4443ac60c64e
Prince1130-sam <princekuldeep2022@gmail.com>
2026-05-15 10:10:49 +0530

    table add

---
+++ Phase_1_WhileLoop/Q.5.js
@@ -0,0 +1,10 @@
+// Print the multiplication table of a given number from n × 1 to n × 10. 
+ 
+let x = 1
+let n =2;
+
+while(x <= 10){
+    let z = n * x;
+    console.log(n ,"x", x ,"=", z)
+    x++;
+}
