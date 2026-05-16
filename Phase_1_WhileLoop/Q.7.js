c9377f33450224ebf7a23cd1ab76e7145c7b31d8
Prince1130-sam <princekuldeep2022@gmail.com>
2026-05-16 11:26:28 +0530

    even number

---
+++ Phase_1_WhileLoop/Q.7.js
@@ -0,0 +1,16 @@
+//  7. Calculate the sum of all even numbers from 1 up to n. 
+
+let n = 1
+let z = 0
+
+
+while(n <= 100){
+    if(n % 2 == 0){
+        z += n
+        
+
+    }
+    n++;
+}
+
+console.log(z)
